08ac8d16ac166d95a9f02c4cc39edc59248cc1e6
Preethahi <murugesanpreetha@gmail.com>
2026-04-14 20:28:24 +0530

    Create 5Mar_02_Factorial_Calculation-TypeScript.ts

---
+++ parent-folder/child-folder/5Mar_02_Factorial_Calculation-TypeScript.ts
@@ -0,0 +1,14 @@
+function factorial(n: number): number {
+    if (n < 0) {
+        console.log("Factorial is not defined for negative numbers.");
+        return 0; // or throw error
+    }
+
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
