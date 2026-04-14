6363bc7ada3897d2c78dc7a5fa053a60362c37d7
Preethahi <murugesanpreetha@gmail.com>
2026-04-14 21:14:15 +0530

    Update 25Mar_Contact.spec.ts

--- parent-folder/child-folder/25Mar_Contact.spec.ts
+++ parent-folder/child-folder/25Mar_Contact.spec.ts
@@ -11,7 +11,7 @@ let tokenType : any
 
 test(`Salesforce Contact`, async ({ page,request }) => {
 
-    await page.goto('https://orgfarm-a0bc339075-dev-ed.develop.lightning.force.com/lightning/n/devedapp__Welcome');
+    await page.goto('https://xxxxxxxxxxxxxxxxx-dev-ed.develop.lightning.force.com/lightning/n/devedapp__Welcome');
     await page.getByTitle("App Launcher",{exact:true}).click();
     await page.getByRole("button",{name:"View All Applications"}).click();
     await page.getByPlaceholder("Search apps or items...",{exact:true}).fill("Contacts");
